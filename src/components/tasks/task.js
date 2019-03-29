18e8ec6a5e3a4bcef5f52254fd8b5bba733fdcce
Akash mishra <mishraakash273694@gmail.com>
2019-03-30 00:51:11 +0530

    fix:ClearFix For Expiry Date & Creation Date

--- src/components/tasks/task.js
+++ src/components/tasks/task.js
@@ -1,29 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './task.css';
 
 class Post extends Component {
     state = {  }
     render() { 
         return ( 
-            <div className="container-fluid">
-                <div className="card no-radius bg-dark border-dark">
-                    <div className="card-body">
-                        <h5 className="card-title">
-                            Title
-                        </h5>
-                        <p className="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's content.
-                        </p>
-                        <button type="button" className="closeButton btn btn-outline-dark">
-                                &times;
-                        </button>
-                    </div>
-                    <div>
+            this.props.tasks.map(data=>{
+                return (
+                    <div className="container-fluid" key={data.id}>
+                        <div className="card no-radius bg-dark border-dark">
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    {data.title}
+                                </h5>
+                                <p className="card-text">
+                                    {data.description}
+                                </p>
+                                <button type="button" className="closeButton btn btn-outline-dark">
+                                        &times;
+                                </button>
+                            </div>
+                            <div class="card-footer clearfix ">
+                                    <span className="">
+                                            Creation Date: {
+                                                new Date(data.expiryDate).getDate() +" / " + new Date(data.expiryDate).getMonth() + " / "+ new Date(data.expiryDate).getFullYear()
+                                            }
+                                    </span>
+                                    <span className="float-right">
+                                            Expiry Date: {
+                                                new Date(data.expiryDate).getDate() +" / " + new Date(data.expiryDate).getMonth() + " / "+ new Date(data.expiryDate).getFullYear()
+                                            }
+                                    </span>
+                                {/* <span className="float-right">
+                                    Expiry Date: {(new Date(data.expiryDate)).toString()}
+                                </span>
+                                 */}
+                            </div>
                         </div>
-                </div>
-            </div>
+                    </div>
+                )
+            })
          );
     }
 }
  
-export default Post;
+var mapToProperty=(state)=>{
+    return {
+        tasks:state.tasks
+    }
+}
+export default connect(mapToProperty,null)(Post);
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,5 +1,13 @@
 var initialState={
-    tasks:['a'],
+    tasks:[
+        {
+            title:'Title of Task',
+            description : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+            expiryDate:(+new Date),
+            creationDate:(+new Date),
+            id:"akadlkfjda"
+        }
+    ],
     completedTask:[]
 }
 var reducer=(state=initialState,action)=>{
