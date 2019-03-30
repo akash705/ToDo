39b9726b6d27c983688bbe25c40c95a7175e6cd3
Akash mishra <mishraakash273694@gmail.com>
2019-03-30 13:48:42 +0530

    added:LocalStorage support

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,17 +1,31 @@
 import actions from './actions';
+const updateLocalStorage=(data)=>{
+        localStorage.setItem('posts',JSON.stringify(data));
+        return true;
+}
+//  {
+//     title:'Title of Task',
+//     description : "Some quick example text to build on the card title and make up the bulk of the card's content.",
+//     expiryDate:(+new Date()),
+//     creationDate:(+new Date()),
+//     id:+new Date(),
+//     status:'active'
+// }
+const getLocalStorage=()=>{
+    try{
+        let a = localStorage.getItem('posts');
+        let b = JSON.parse(a);
+        return b||[];
+    }
+    catch(e){
+        return [];
+    }
+}
 var initialState={
-    tasks:[
-        {
-            title:'Title of Task',
-            description : "Some quick example text to build on the card title and make up the bulk of the card's content.",
-            expiryDate:(+new Date()),
-            creationDate:(+new Date()),
-            id:+new Date(),
-            status:'active'
-        }
-    ],
+    tasks:[],
     completedTask:[]
 }
+initialState.tasks=getLocalStorage();
 var reducer=(state=initialState,action)=>{
     switch(action.type){
         case(actions.CREATED):{
@@ -19,6 +33,7 @@ var reducer=(state=initialState,action)=>{
             action.data.id=+new Date();
             action.data.creationDate=+new Date();
             originalPost.push(action.data);
+            updateLocalStorage(originalPost);
             return {
                 ...state,
                 tasks:originalPost
@@ -27,6 +42,7 @@ var reducer=(state=initialState,action)=>{
         case(actions.DELETED):{
             let originalPost=[...state.tasks];
             originalPost.splice(action.data.index,1);
+            updateLocalStorage(originalPost);
             return {
                 ...state,
                tasks: originalPost
@@ -38,6 +54,7 @@ var reducer=(state=initialState,action)=>{
             originalPost[action.data.index]=action.data.data;
             originalPost[action.data.index].creationDate=originPost.creationDate;
             originalPost[action.data.index].id=originPost.id;
+            updateLocalStorage(originalPost);
             return {
                 ...state,
                 tasks:originalPost
